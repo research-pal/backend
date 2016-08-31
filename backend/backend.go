d03c49f75b39755e57fa2dfab08785b3cd26547e
srinivasa reddy muly <msrinivasareddy@gmail.com>
2016-08-31 01:18:46 -0400

    backend: initial file with some notes on the todo items

---
+++ backend/backend.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+// TODO:
+//// get/post api endpoint: if url (passed in request body) is already available in database, return that record, otherwise create a record for that url and return that record
+//// put api endpoint: update/overwrite the record in the database for the given url
+//// struct fields: url, notes (plain text for now, but will be html soon)
+
+// Challanges:
